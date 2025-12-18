2a7463924f57e2445ab3f1901287f0e817a2d853
vmatsuda <majtyan@gmail.com>
2025-12-18 17:41:10 +0900

    after review fix

---
+++ philo/src/free_utils.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free_utils.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vmatsuda <vmatsuda@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/16 20:33:04 by vmatsuda          #+#    #+#             */
+/*   Updated: 2025/12/18 15:30:47 by vmatsuda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	free_exit(t_all *all, int *args, int exit_status)
+{
+	free_all(all, *args);
+	free(args);
+	return (exit_status);
+}
+
+int	free_all(t_all *all, int n)
+{
+	int	i;
+	
+	pthread_mutex_destroy(&all->time_mtx);
+	// pthread_mutex_destroy(&all->eat_limit);
+	pthread_mutex_destroy(&all->meal_mtx);
+	pthread_mutex_destroy(&all->print_mtx);
+	free(all->ph_thread_pool);
+	if (all->forks)
+	{
+		i = n;
+		while (i-- > 0)
+			pthread_mutex_destroy(&all->forks[i]);
+		free(all->forks);
+	}
+	free(all->philos);
+	return (1);
+}
---
+++ philo/src/routine_do.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   routine_do.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vmatsuda <vmatsuda@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/11 11:25:31 by vmatsuda          #+#    #+#             */
+/*   Updated: 2025/12/18 17:12:58 by vmatsuda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	*do_monitoring(void *arg)
+{
+	t_all	*all;
+	size_t	i;
+
+	all = (t_all *)arg;
+	while (1)
+	{
+		i = -1;
+		all->ph_finished = 0;
+		if (check_die(&all->philos[i]))
+			return (NULL);
+		while (++i < all->philos_count)
+		{
+			if (check_eaten_meal(&all->philos[i]))
+				all->ph_finished++;
+		}
+		if (all->ph_finished == all->philos_count)
+			return (NULL);
+	}
+	return (NULL);
+}
+
+void	*do_action(void *arg)
+{
+	t_philo	*philo;
+
+	philo = (t_philo *)arg;
+	if (philo->all->philos_count == 1)
+	{
+		print_message(philo, get_elapsed_time(philo->time_created), FORK);
+		return (NULL);
+	}
+	if (philo->id % 2 == 0)
+		ft_usleep(philo->time_to_eat / 2, philo);
+	while (1)
+	{
+		if (exit_dead_flag(philo))
+			return (NULL);
+		if (check_eaten_meal(philo))
+			return (NULL);
+		do_eat(philo);
+		if (exit_dead_flag(philo))
+			return (NULL);
+		do_sleep(philo);
+		if (exit_dead_flag(philo))
+			return (NULL);
+		do_think(philo);
+	}
+	return (NULL);
+}
+
+void	do_eat(t_philo *philo)
+{
+	pthread_mutex_lock(philo->lfork_mtx);
+	print_message(philo, get_elapsed_time(philo->time_created), FORK);
+	pthread_mutex_lock(philo->rfork_mtx);
+	print_message(philo, get_elapsed_time(philo->time_created), FORK);
+	set_time_last_meal(philo);
+	print_message(philo, get_elapsed_time(philo->time_created), EAT);
+	ft_usleep(philo->time_to_eat, philo);
+	increment_eaten_meal(philo);
+	pthread_mutex_unlock(philo->lfork_mtx);
+	pthread_mutex_unlock(philo->rfork_mtx);
+}
+
+void	do_sleep(t_philo *philo)
+{
+	print_message(philo, get_elapsed_time(philo->time_created), SLEEP);
+	ft_usleep(philo->time_to_sleep, philo);
+}
+
+void	do_think(t_philo *philo)
+{
+	print_message(philo, get_elapsed_time(philo->time_created), THINK);
+}
